f322820935fbb2331d069ab3ce6a01438fd5f8d1
Linxerz <58645425+Linxerz@users.noreply.github.com>
2023-07-04 00:03:57 -0500

    Time: 1392 ms (5.01%), Space: 17.4 MB (11.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,8 +2,8 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        s = [*s]
-        t = [*t]
+        s = list(s)
+        t = list(t)
         for letter in s:
             if letter in t:
                 t.remove(letter)
